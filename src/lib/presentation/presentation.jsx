4a680d682c990c2d6f7a5ae5dc171044c2ba5657
Felipe <luisfelipe7014feups@gmail.com>
2026-02-06 21:24:45 -0300

    feat: Presentation structured and stylized

---
+++ src/lib/presentation/presentation.jsx
@@ -0,0 +1,51 @@
+import fundoGrade from "../../assets/grad-preta.png"
+import felipe from "../../assets/felipe2.png"
+
+function Presentation() {
+
+    return (
+        <>
+            <div id="background" className="absolute bg-white">
+                <img src={fundoGrade} alt="Fundo Grade" className='mix-blend-exclusion opacity-45' />
+            </div>
+
+            <div id='content-presentation' className="flex z-10 w-full">
+                <aside id='left' className="relative flex-1">
+                    <h1 className="relative text-black font-kanit text-[80px] leading-[0.9] font-extralight pl-25 pt-35">
+                        Prazer,
+                    </h1>
+                    <h1 className="relative text-black font-kanit text-[80px] leading-[1.2] font-semibold pl-45 
+                    tracking-tight bg-linear-to-t from-mid-blue to-dark-blue bg-clip-text text-transparent">
+                        Felipe Jorge
+                    </h1>
+                    <p className="relative text-black font-kanit font-extralight text-[30px] pl-25 pt-20 pb-10 w-315 text-base/8">
+                        Designer gráfico com 4 anos de experiência, especializado em social media e identidade visual.
+                        Ao longo da minha trajetória, desenvolvi projetos que fortaleceram marcas, ampliaram presença
+                        online e ajudaram empresas e profissionais a se posicionarem com estratégia e consistência..
+                    </p>
+                    <p className="relative text-black font-kanit font-extralight text-[30px] pl-25  pb-10 w-315 text-base/8">
+                        Atuei também em uma das maiores agências da minha cidade, a Diamond, onde tive contato com
+                        projetos de alto volume e alto padrão, refinando ainda mais meu olhar criativo e minha
+                        capacidade de entregar resultados sólidos.
+                    </p>
+                    <p className="relative text-black font-kanit font-extralight text-[30px] pl-25  pb-10 w-315 text-base/8">
+                        Tenho 2 anos de experiência no setor político, contribuindo para campanhas e comunicação
+                        institucional, incluindo uma vitória em campanha para prefeito, fruto de um trabalho
+                        estratégico, claro e direcionado ao público
+                    </p>
+                    <p className="relative text-dark-blue font-kanit font-bold text-[30px] pl-25 pt-10 w-315 text-base/8">
+                        Hoje, meu foco é transformar ideias em visuais
+                    </p>
+                    <p className="relative text-mid-blue font-kanit font-bold text-[30px] pl-25 w-315 text-base/8">
+                        que conectam, engajam e geram impacto real.
+                    </p>
+                </aside>
+                <aside id="right" className="relative pr-20 pt-50">
+                    <img src={felipe} alt="Foto de Felipe Jorge" className="w-130 rounded-[100px]" />
+                </aside>
+            </div>
+        </>
+    )
+}
+
+export default Presentation
--- src/screens/template.jsx
+++ src/screens/template.jsx
@@ -1,12 +1,18 @@
 import InitialHomeAvulso from "../lib/home/avulsos/initialHomeAvulso"
 import InitialHomePackege from "../lib/home/packages/initialHomePackage"
+import Presentation from "../lib/presentation/presentation"
 function Template({ tipo }) {
 
     return (
         <>
+
             <section id="home-section" className="w-full h-full">
                 {tipo === 'avulsos' ? <InitialHomeAvulso /> : <InitialHomePackege />}
             </section>
+            <section id="presentation-section" className="relative w-full min-h-screen ">
+                <Presentation />
+            </section>
+
         </>
     )
 }
