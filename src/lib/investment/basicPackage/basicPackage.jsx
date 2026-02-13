39c0c9913c75ca7165f4518c7180ae37993f22b5
Felipe <luisfelipe7014feups@gmail.com>
2026-02-13 12:05:50 -0300

    feat: Prices page Avulsos

--- src/lib/investment/basicPackage/basicPackage.jsx
+++ src/lib/investment/basicPackage/basicPackage.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
+import fundoGrade from "../../../assets/grad-preta.png"
+import triangulo from "../../../assets/triangulo.png"
+import vector from "../../../assets/Vector.png"
+import SlideInLeft from '../../../animations/slideInLeft';
+import SlideInBottom from '../../../animations/slideInBottom';
 
 function BasicPackage() {
-
     const [pacotes, setPacotes] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // URL da sua planilha via OpenSheet
         const url = "https://opensheet.elk.sh/1_r21VCtPgOCeudcsY1PqKANn1Kgg_BiFx8hWXUVYu30/Página1!A3:Z";
 
         fetch(url)
@@ -21,18 +24,68 @@ function BasicPackage() {
             });
     }, []);
 
-    if (loading) return <p>Carregando propostas...</p>;
 
     return (
         <>
-            <div style={{ display: 'flex', gap: '20px', padding: '20px' }}>
-                {pacotes.map((item, index) => (
-                    <div key={index} style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '8px' }}>
-                        <h2>{item["Quantidade postagens"]} Postagens</h2>
-                        <p>Carrosséis: {item["Carrosseis"]}</p>
-                        <p><strong>Preço: R$ {item["Preco"]}</strong></p>
+            <div id='body' className='relative'>
+
+                <div id="background" className="absolute bg-white">
+                    <img src={fundoGrade} alt="Fundo Grade" className='mix-blend-exclusion opacity-45' />
+                </div>
+
+                <div id='content' className='absolute'>
+                    <SlideInLeft delay={0.2}>
+                        <div id='tittle' className='pl-45 pt-19 flex items-center'>
+                            <img className='h-5 w-5 ' src={triangulo} alt="Triangulo" />
+                            <h1 className='relative  text-black font-kanit text-[80px]  font-semibold 
+                        tracking-tight bg-linear-to-t from-mid-blue to-dark-blue bg-clip-text text-transparent'>
+                                Pacotes</h1>
+                        </div>
+                    </SlideInLeft>
+
+
+                    <div id='itens' className='columns-2 pl-15 pt-5'>
+                        {pacotes.map((item, index) => (
+                            <div key={index} className='flex flex-col font-kanit font-extralight text-[30px] gap-2 mb-8 
+                                break-inside-avoid'>
+                                <div className="flex items-center gap-4 pl-35">
+                                    <SlideInBottom delay={0.6}>
+                                        <img src={vector} alt="Icone de correto" className='w-4 h-4' />
+                                    </SlideInBottom>
+
+
+                                    <SlideInBottom delay={0.6}>
+                                        <h2 className='pr-16'>{item["Quantidade postagens"]} Postagens semanais</h2>
+                                    </SlideInBottom>
+
+
+                                    <SlideInBottom delay={0.7}>
+                                        <div className='text-white font-kanit text-[20px] font-medium p-2 rounded-[10px] 
+                                        bg-linear-to-t from-mid-blue to-dark-blue flex items-center justify-center'>
+                                            <p>R$ {item["Preco"]},00</p>
+                                        </div>
+                                    </SlideInBottom>
+
+                                </div>
+                                <SlideInBottom delay={0.8}>
+                                    <p className="pl-35  text-[20px] bg-linear-to-t from-mid-blue to-dark-blue bg-clip-text 
+                                    text-transparent ">( {item["Carrosseis"]}  Podendo ser carrossel ) </p>
+                                </SlideInBottom>
+
+                            </div>
+
+
+                        ))}
                     </div>
-                ))}
+
+                    <SlideInLeft delay={0.9} >
+                        <p className='font-kanit font-light text-[26px] pl-45 pt-8'> <span className='text-mid-blue'>
+                            Obs:</span> Caso queira mais posts em formato carrossel, me <br /> informe para que eu possa
+                            reformular o orçamento.</p>
+                    </SlideInLeft>
+
+                </div>
+
             </div>
         </>
     )
